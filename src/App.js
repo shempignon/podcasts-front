5e94779632b5fa4c33b0a2f2a7d9b06ff9b2ce5d
Nicolas Saignes <nicolas@pointndots.com>
2018-10-15 22:55:01 +0200

    Add Apollo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Query} from 'react-apollo';
+import gql from 'graphql-tag';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const GET_BROADCASTS = gql`
+{
+  broadcasts {
+    edges {
+      node {
+        name
+        cover {
+          url
+          download {
+            path
+          }
+        }
+      }
+    }
   }
 }
+`;
+
+const App = () => (
+  <Query query={GET_BROADCASTS}>
+    {({loading, error, data}) => {
+      if (loading) return 'Loading...';
+      if (error) return `Error! ${error.message}`;
+
+      return (
+        <ul>
+          {data.broadcasts.edges.map(broadcast => (
+            <li key={broadcast.node.name}>{broadcast.node.name}</li>
+          ))}
+        </ul>
+      );
+    }}
+  </Query>
+);
 
 export default App;
---
+++ src/client.js
@@ -0,0 +1,16 @@
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const uri = process.env.REACT_APP_GRAPHQL_URI;
+
+const client = new ApolloClient({
+    // By default, this client will send queries to the
+    //  `/graphql` endpoint on the same host
+    // Pass the configuration option { uri: YOUR_GRAPHQL_API_URL } to the `HttpLink` to connect
+    // to a different host
+    link: new HttpLink({ uri }),
+    cache: new InMemoryCache(),
+});
+
+export default client;
